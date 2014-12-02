acb76283bfca6f46e4d883d6109c51d256f31ceb
Brian Weaver <cmdrclueless@gmail.com>
2014-12-02 17:22:14 -0500

    Learning with my feet in the fire

--- honeyd/debian/honeyd.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the honeyd package
-#
-0 4	* * *	root	[ -x /usr/bin/honeyd_maintenance ] && /usr/bin/honeyd_maintenance
--- honeyd/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(honeyd):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="honeyd" command="/usr/bin/honeyd"
