08aa0984e3b8a938bf679cfd0d05c4be7cc611ee
Brian Weaver <cmdrclueless@gmail.com>
2014-12-02 15:42:29 -0500

    Initial Debian Framework

---
+++ honeyd/debian/honeyd.default.ex
@@ -0,0 +1,10 @@
+# Defaults for honeyd initscript
+# sourced by /etc/init.d/honeyd
+# installed at /etc/default/honeyd by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ honeyd/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(honeyd):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="honeyd" command="/usr/bin/honeyd"
