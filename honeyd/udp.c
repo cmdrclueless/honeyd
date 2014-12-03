91f518cffc86fec6ac765c0a0e1010ba92e1931e
Brian Weaver <cmdrclueless@gmail.com>
2014-12-02 23:09:41 -0500

    Handle large UDP packets

--- honeyd/udp.c
+++ honeyd/udp.c
@@ -49,7 +49,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <dnet.h>
+#include <dumbnet.h>
 
 #undef timeout_pending
 #undef timeout_initialized
@@ -126,11 +126,20 @@ udp_add_readbuf(struct udp_con *con, u_char *dat, u_int datlen)
 void
 cmd_udp_read(int fd, short which, void *arg)
 {
+	static u_char *buf = NULL;
+	static int bufsz = 1<<16;
+
 	struct udp_con *con = arg;
-	u_char buf[2048];
 	ssize_t len;
 
-	TRACE(fd, len = read(fd, buf, sizeof(buf)));
+	if (!buf) {
+		buf = malloc(bufsz); /* largest possible UDP packet */
+		if (!buf)
+			return;
+	}
+	memset(buf, 0, bufsz);
+
+	TRACE(fd, len = recv(fd, buf, bufsz, 0));
 	if (len == -1) {
 		if (errno == EINTR || errno == EAGAIN)
 			goto again;
@@ -156,7 +165,7 @@ cmd_udp_write(int fd, short which, void *arg)
 	buf = TAILQ_FIRST(&con->incoming);
 	if (buf == NULL)
 		return;
-	TRACE(fd, len = write(fd, buf->buf, buf->len));
+	TRACE(fd, len = send(fd, buf->buf, buf->len, 0));
 	if (len == -1) {
 		if (errno == EINTR || errno == EAGAIN)
 			goto again;
